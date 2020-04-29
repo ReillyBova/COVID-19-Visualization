497ce6313d727e686010b92ad5a337f69e066f6e
Reilly Bova <rbova@princeton.edu>
2020-04-29 04:29:19 -0400

    Fixed build flow

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, buildPath),
         filename: '[name].[hash].js',
-        publicPath: `/${pkg.repo}/`
+        publicPath: `/${pkg.repository}/`
     },
     target: 'web',
     devtool: 'source-map',
